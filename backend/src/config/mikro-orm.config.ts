0f142f6c9591b8ef85e7f725114979dc91f2d319
grahil-24 <rahilganatra@gmail.com>
2025-12-11 15:36:04 +0530

    added user table to database and mikroorm

--- backend/src/config/mikro-orm.config.ts
+++ backend/src/config/mikro-orm.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "@mikro-orm/mysql";
 import { Car } from "../entities/Car";
+import {User} from '../entities/User';
 
 export default defineConfig({
-  entities: [Car],       // all entities go here
+  entities: [Car, User],       // all entities go here
   dbName: process.env.MYSQL_DB,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
---
+++ backend/src/entities/User.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryKey, Property, SmallIntType, Unique} from "@mikro-orm/core";
+
+@Entity({tableName: "users"})
+export class User{
+    @PrimaryKey({type: "smallint", unsigned: true, autoincrement: true, nullable: false})
+    user_id!:SmallIntType
+
+    @Unique()
+    @Property({type: "varchar", nullable: false, length: 255})
+    email!:string
+
+    @Property({type: "char", length: 60, nullable: false})
+    password!:string
+} 
