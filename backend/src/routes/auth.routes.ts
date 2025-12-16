802f1a5131f663cc57dc7a8a790bd734883ea677
grahil-24 <rahilganatra@gmail.com>
2025-12-16 15:53:02 +0530

    added routes to verify access and refresh token

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import {signUp, login} from '../controllers/auth.controller';
+import {signUp, login, refresh, verify} from '../controllers/auth.controller';
 
 const router = express.Router();
 
 router.post('/sign-up', signUp);
 router.post('/login', login);
+router.get('/refresh', refresh);
+router.get('/verify', verify);
 
 export default router;
