e36ce97320eb073beae653b4486247949322cd67
grahil-24 <rahilganatra@gmail.com>
2026-03-07 18:31:25 +0530

    set samesite to none

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -139,8 +139,6 @@ async function start(){
             cookie: {
                 httpOnly: process.env.NODE_ENV === 'production',
                 secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                maxAge: 24 * 60 * 60 * 1000 // 24 hours
             },
             name: 'adminjs',
         }
