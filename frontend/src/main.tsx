68f08594d51ec4df4e10db0aa49a03d2e1916541
grahil-24 <rahilganatra@gmail.com>
2026-03-13 09:41:40 +0530

    analytics for vercel frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer, Slide } from 'react-toastify';
 import { LoadingBarProvider } from './contexts/LoadingBarProvider.tsx';
 import './toast.style.css';
+import {Analytics} from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <LoadingBarProvider>
         <ToastContainer hideProgressBar={true} pauseOnHover={false} transition={Slide} pauseOnFocusLoss={false} position='top-center' autoClose={5000} />
+        <Analytics />
         <App />
         </LoadingBarProvider>
       </AuthProvider>
